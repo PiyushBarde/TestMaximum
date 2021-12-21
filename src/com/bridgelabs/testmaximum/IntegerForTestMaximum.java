6db34f50bd613026e59d7336a3900b33c10a806e
PiyushBarde <piyushbarde1@gmail.com>
2021-12-21 15:23:50 +0530

    UPDATED: maximum amoung 3 float values UC2

--- src/com/bridgelabs/testmaximum/IntegerForTestMaximum.java
+++ src/com/bridgelabs/testmaximum/IntegerForTestMaximum.java
@@ -14,7 +14,7 @@ public class IntegerForTestMaximum {
 	}
 	
 	static  void calculatingMaximumInt(Integer subNum1,Integer subNum2,Integer subNum3) {
-		if((TestMaximum.num1).compareTo(TestMaximum.num2) == 1 && (TestMaximum.num1).compareTo(TestMaximum.num2) == 1)
+		if((TestMaximum.num1).compareTo(TestMaximum.num2) == 1 && (TestMaximum.num1).compareTo(TestMaximum.num3) == 1)
 			{
 				System.out.println("Maximum integer is = " + TestMaximum.num1 );
 			}
@@ -31,5 +31,7 @@ public class IntegerForTestMaximum {
 	public static void main(String[] args) {
 		integerForTestMaximum();
 		calculatingMaximumInt(TestMaximum.num1,TestMaximum.num2,TestMaximum.num3);
+		FloatForTestMaximum.floatForTestMaximum();
+		FloatForTestMaximum.calculatingMaximumFloat();
 	}
 }
--- src/com/bridgelabs/testmaximum/TestMaximum.java
+++ src/com/bridgelabs/testmaximum/TestMaximum.java
@@ -3,10 +3,21 @@ import java.util.Scanner;
 public class TestMaximum {
 	static Integer num1,num2,num3;
 	
+	
 	public TestMaximum(Integer num1, Integer num2, Integer num3) {
 		super();
 		this.num1 = num1;
 		this.num2 = num2;
 		this.num3 = num3;
 	}
+public static class TestMaximumFloat{
+	static Float numF1, numF2, numF3;
+	public TestMaximumFloat(Float numF1, Float numF2, Float numF3) {
+		super();
+		this.numF1 = numF1;
+		this.numF2 = numF2;
+		this.numF3 = numF3;
+	}
+	
+}
 }
