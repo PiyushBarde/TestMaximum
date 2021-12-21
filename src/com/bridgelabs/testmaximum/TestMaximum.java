c2f61803f2fa2008bb172e7f2c9ea8a010e4b0b4
PiyushBarde <piyushbarde1@gmail.com>
2021-12-21 15:05:33 +0530

    ADDED: maximum amoung three integers UC1

---
+++ src/com/bridgelabs/testmaximum/TestMaximum.java
@@ -0,0 +1,12 @@
+package com.bridgelabs.testmaximum;
+import java.util.Scanner;
+public class TestMaximum {
+	static Integer num1,num2,num3;
+	
+	public TestMaximum(Integer num1, Integer num2, Integer num3) {
+		super();
+		this.num1 = num1;
+		this.num2 = num2;
+		this.num3 = num3;
+	}
+}
